658ad8c1ae5c48ce8032835f7a24af28d259546b
willcl-ark <will@256k1.dev>
2024-08-07 10:35:01 +0100

    add promtail stdout scraper

--- resources/scripts/install_logging.sh
+++ resources/scripts/install_logging.sh
@@ -9,6 +9,6 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm repo update
 
 helm upgrade --install --namespace warnet-logging --create-namespace --values "${MANIFESTS_DIR}/loki_values.yaml" loki grafana/loki --version 5.47.2
-helm upgrade --install --namespace warnet-logging promtail grafana/promtail
+helm upgrade --install --namespace warnet-logging promtail grafana/promtail --values "${MANIFESTS_DIR}/promtail-values.yaml"
 helm upgrade --install --namespace warnet-logging prometheus prometheus-community/kube-prometheus-stack --namespace warnet-logging --set grafana.enabled=false
 helm upgrade --install --namespace warnet-logging loki-grafana grafana/grafana --values "${MANIFESTS_DIR}/grafana_values.yaml"
