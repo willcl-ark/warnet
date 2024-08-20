55bd04edaa9a9abc295c7839dfe1855f1e4e5a4a
willcl-ark <will@256k1.dev>
2024-08-20 17:30:06 +0200

    graph: use comma separated config lines in graph

--- src/warnet/cli/network.py
+++ src/warnet/cli/network.py
@@ -222,7 +222,7 @@ zmqpubrawblock=tcp://0.0.0.0:28332
 zmqpubrawtx=tcp://0.0.0.0:28333
 """
     node_specific_config = data.get("bitcoin_config", "")
-    return f"{base_config}\n{node_specific_config}"
+    return f"{base_config}\n{node_specific_config.replace(",", "\n")}"
 
 
 def set_kubectl_context(namespace: str):
